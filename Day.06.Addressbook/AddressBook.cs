48dd3e7efd4a374041e5ba3b97b8f5377d348c04
Ramya.U <ramyayadav2016@gmail.com>
2020-10-26 16:38:10 +0530

    Added UC-1

---
+++ Day.06.Addressbook/AddressBook.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day._06.Addressbook
+{
+    class AddressBook
+    {
+        public void AddContact()
+        {
+            Console.WriteLine("Enter First Name");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Enter Last Name");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Enter your address");
+            string address = Console.ReadLine();
+            Console.WriteLine("Enter your city name");
+            string city = Console.ReadLine();
+            Console.WriteLine("Enter your state");
+            string state = Console.ReadLine();
+            Console.WriteLine("Enter your zip code");
+            string zipCode = Console.ReadLine();
+            Console.WriteLine("Enter your phone number");
+            int phoneNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your Email-Id");
+            string email = Console.ReadLine();
+
+            ///Creates a reference of Contact class
+            Contact contact = new Contact(firstName, lastName, address, city, state, zipCode, phoneNumber, email);
+            Console.WriteLine(contact);
+        }
+    }
+}
---
+++ Day.06.Addressbook/Contact.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day._06.Addressbook
+{
+    class Contact
+    {
+        //varialbes
+        String firstName;
+        String lastName;
+        String address;
+        String city;
+        String state;
+        String phoneNumber;
+        String zipCode;
+        String email;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Contact" /> class.
+        /// </summary>
+        /// <param name="firstName">The first name.</param>
+        /// <param name="lastName">The last name.</param>
+        /// <param name="address">The address.</param>
+        /// <param name="city">The city.</param>
+        /// <param name="state">The state.</param>
+        /// <param name="phoneNumber">The phone number.</param>
+        /// <param name="zipCode">The zip code.</param>
+        /// <param name="="email">The email.</param>
+        /// </summary>
+
+        public Contact(string firstName, string lastName, string address, string city, string state, string phoneNumber, int phoneNumber1, string zipCode)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.address = address;
+            this.city = city;
+            this.state = state;
+            this.phoneNumber = phoneNumber;
+            this.zipCode = zipCode;
+            this.email = email;
+        }
+                
+        public override string ToString() //this method returns the string itself
+        {
+            return   (  "FirstName : " + firstName + " LastName : " + lastName + " Address : " + address + " City : " + city + "State : " + state +
+            " Zip Code :" + zipCode + " Phone Number : " + phoneNumber + " Email Id : " + email );
+        }
+    }
+}
